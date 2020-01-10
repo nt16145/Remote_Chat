56b9e4c4a808cfb211658fb222866c21d391a2df
nt16145 <nt16145@tomakomai.kosen-ac.jp>
2020-01-10 15:54:53 +0900

    1on1実装テスト

--- public/scripts/skyway.js
+++ public/scripts/skyway.js
@@ -1,4 +1,6 @@
 let localStream = null;
+let peer = null;
+let existingCall = null;
 
 navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(function (stream) {
@@ -8,3 +10,71 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         console.error('mediaDevice.getUserMedia() error:', error);
         return;
     });
+
+peer = new Peer({
+    key: '749f82c3-e9df-4bee-ae42-c3007acc4e2a',
+    debug: 3
+});
+
+peer.on('open', function () {
+    $('#my-id').text(oeer.id);
+});
+
+peer.on('call', function (call) {
+    call.answer(localStream);
+    setupCallEventHandlers(call);
+});
+
+peer.on('error', function (err) {
+    alert(err.message);
+});
+
+$('#make-call').submit(function (e) {
+    e.preventDefault();
+    const call = peer.call($('#peer-id').val(), localStream);
+    setupCallEventHandlers(call);
+});
+
+$('#end-call').click(function () {
+    existingCall.close();
+});
+
+function setupCallEventHandlers(call) {
+    if (existingCall) {
+        existingCall.close();
+    };
+
+    existingCall = call;
+
+    call.on('stream', function (stream) {
+        addVideo(call, stream);
+        setupEndCallUI();
+        $('#connected-peer-id').text(call.remoteId);
+    });
+
+    call.on('close', function () {
+        removeVideo(call.remoteId);
+        setupMakeCallUI();
+    });
+}
+
+function addVideo(call, stream) {
+    const videoDom = $('<video autoplay>');
+    videoDom.attr('id', call.remoteId);
+    videoDom.get(0).srcObject = stream;
+    $('.videosContainer').append(videoDom);
+}
+
+function removeVideo(peerId) {
+    $('#' + peerId).remove();
+}
+
+function setupMakeCallUI() {
+    $('#make-call').show();
+    $('#end-call').hide();
+}
+
+function setupEndCallUI() {
+    $('#make-call').hide();
+    $('#end-call').show();
+}
