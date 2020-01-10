0a18344892aa43923a654c10307c58fffef1b613
nt16145 <nt16145@tomakomai.kosen-ac.jp>
2020-01-10 15:38:55 +0900

    skyway追加

---
+++ public/scripts/skyway.js
@@ -0,0 +1,10 @@
+let localStream = null;
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(function (stream) {
+        $('#myStream').get(0).srcObject = stream;
+        localStream = stream;
+    }).catch(function (error) {
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+    });
