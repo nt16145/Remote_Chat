ff195abd54c4bd80e6a1da62f885388a272fab45
nt16145 <nt16145@tomakomai.kosen-ac.jp>
2020-07-03 14:15:00 +0900

    遠隔第1期終了

--- functions/static/scripts/select_group.js
+++ functions/static/scripts/select_group.js
@@ -1,6 +1,10 @@
 var refer = document.referrer;
-if (!refer.match(/login.html$/) && !refer.match(/group_register.html$/) && !refer.match(/enter_group.html$/)) {
-  window.location.href = 'not_allow.html'
+if (
+  !refer.match(/login.html$/) &&
+  !refer.match(/group_register.html$/) &&
+  !refer.match(/enter_group.html$/)
+) {
+  window.location.href = "not_allow.html";
 }
 
 var firestore = firebase.firestore();
@@ -8,10 +12,13 @@ var firestore = firebase.firestore();
 function getGrouplist() {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      firestore.collection('groups').where('groupOwner', '==', user.uid).get()
+      firestore
+        .collection("groups")
+        .where("groupOwner", "==", user.uid)
+        .get()
         .then(function (querySnapshot) {
           querySnapshot.forEach(function (doc) {
-            let template = document.querySelector('#groupTemplate');
+            let template = document.querySelector("#groupTemplate");
             let tbody = document.querySelector("tbody");
             let clone = document.importNode(template.content, true);
             let th = clone.querySelector("th");
@@ -21,11 +28,14 @@ function getGrouplist() {
             tbody.appendChild(clone);
           });
         });
-      firestore.collection('groups').where('groupMember', '==', user.uid).get()
+      firestore
+        .collection("groups")
+        .where("groupMember", "==", user.uid)
+        .get()
         .then(function (querySnapshot) {
           console.log(querySnapshot.size);
           querySnapshot.forEach(function (doc) {
-            let template = document.querySelector('#groupTemplate');
+            let template = document.querySelector("#groupTemplate");
             let tbody = document.querySelector("tbody");
             let clone = document.importNode(template.content, true);
             let th = clone.querySelector("th");
@@ -36,21 +46,23 @@ function getGrouplist() {
           });
         });
     } else {
-      window.location.href = 'not_allow.html'
+      window.location.href = "not_allow.html";
     }
   });
 }
 
 getGrouplist();
 
-document.addEventListener('click', function (e) {
+document.addEventListener("click", function (e) {
   var t = e.target;
   if (t.nodeName == "INPUT" && t.type == "button") {
-    var a = Array.prototype.map.call(t.parentNode.parentNode.querySelectorAll('td'), function (n) {
-      return n.textContent;
-    }).filter(function (v) {
-      return v !== "";
-    });
+    var a = Array.prototype.map
+      .call(t.parentNode.parentNode.querySelectorAll("td"), function (n) {
+        return n.textContent;
+      })
+      .filter(function (v) {
+        return v !== "";
+      });
     console.log(a);
   }
 });
